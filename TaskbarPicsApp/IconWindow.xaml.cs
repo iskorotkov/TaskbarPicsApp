a498ab23356296ab92d6e64e55b2761f39ca7f90
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-05-23 19:29:44 +0500

    Add notify icon

---
+++ TaskbarPicsApp/IconWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TaskbarPicsApp
+{
+    /// <summary>
+    /// Interaction logic for IconWindow.xaml
+    /// </summary>
+    public partial class IconWindow
+    {
+        private NotifyIcon _notifyIcon;
+
+        public IconWindow()
+        {
+            InitializeComponent();
+            _notifyIcon = new NotifyIcon
+            {
+                BalloonTipText = Properties.Resources.IconWindow_IconWindow_Hello_world_,
+                BalloonTipTitle = Properties.Resources.IconWindow_IconWindow_The_App,
+                Text = Properties.Resources.IconWindow_IconWindow_Text,
+                Icon = Properties.Resources.TaskbarIcon
+            };
+            _notifyIcon.Click += NotifyIcon_Click;
+            ShowTrayIcon(true);
+            _notifyIcon.ShowBalloonTip(2000);
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ShowTrayIcon(bool show)
+        {
+            if (_notifyIcon != null)
+            {
+                _notifyIcon.Visible = show;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+    }
+}
--- TaskbarPicsApp/Properties/Resources.Designer.cs
+++ TaskbarPicsApp/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TaskbarPicsApp.Properties
-{
-
-
+namespace TaskbarPicsApp.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,81 @@ namespace TaskbarPicsApp.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TaskbarPicsApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello world!.
+        /// </summary>
+        internal static string IconWindow_IconWindow_Hello_world_ {
+            get {
+                return ResourceManager.GetString("IconWindow_IconWindow_Hello_world_", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Text.
+        /// </summary>
+        internal static string IconWindow_IconWindow_Text {
+            get {
+                return ResourceManager.GetString("IconWindow_IconWindow_Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The App.
+        /// </summary>
+        internal static string IconWindow_IconWindow_The_App {
+            get {
+                return ResourceManager.GetString("IconWindow_IconWindow_The_App", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon TaskbarIcon {
+            get {
+                object obj = ResourceManager.GetObject("TaskbarIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
     }
